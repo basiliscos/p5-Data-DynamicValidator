afffa2adedee6aa5887fc5d87fa133675aa4423a
Ivan Baidakou <dmol@gmx.com>
2014-02-03 20:24:52 +0300

    Build results of 5f737a3 (on master)

--- lib/Data/DynamicValidator.pm
+++ lib/Data/DynamicValidator.pm
@@ -1,6 +1,6 @@
 package Data::DynamicValidator;
 {
-  $Data::DynamicValidator::VERSION = '0.01';
+  $Data::DynamicValidator::VERSION = '0.02';
 }
 #ABSTRACT: JPointer-like and Perl union for flexible perl data structures validation
 
@@ -289,7 +289,7 @@ Data::DynamicValidator - JPointer-like and Perl union for flexible perl data str
 
 =head1 VERSION
 
-version 0.01
+version 0.02
 
 =head1 SYNOPSIS
 
